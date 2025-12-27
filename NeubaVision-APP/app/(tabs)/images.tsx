a501efe0ea5062229b7767a18ac1ede5e48c9396
MateoMor <mateomorales5558@gmail.com>
2025-12-27 09:36:55 -0500

    💄 Style: add visual icon of selected item

--- NeubaVision-APP/app/(tabs)/images.tsx
+++ NeubaVision-APP/app/(tabs)/images.tsx
@@ -1,23 +1,18 @@
 import React, { useState } from "react";
-import {
-  FlatList,
-  View,
-  LayoutChangeEvent,
-  Modal,
-  Pressable,
-  Text,
-  useWindowDimensions,
-} from "react-native";
+import { FlatList, View, LayoutChangeEvent, Pressable } from "react-native";
 import { usePhotosStore } from "@/store/usePhotosStore";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ImageWithBoundingBoxes } from "@/components/tab_images/ImageWithBoundingBoxes";
 import { Ionicons } from "@expo/vector-icons";
 import { ImageOptionsModal } from "@/components/tab_images/ImageOptionsModal";
+import { ProcessedPhotoData } from "@/types/ProcessedPhotoData";
 
 export default function ImagesScreen() {
-  const { detections } = usePhotosStore();
+  const detections = usePhotosStore((state) => state.detections);
   const [itemSize, setItemSize] = useState(0);
-  const [selectedImage, setSelectedImage] = useState<[string, any] | null>(null);
+  const [selectedImage, setSelectedImage] = useState<[string, ProcessedPhotoData] | null>(
+    null
+  );
 
   const GAP = 4;
   const COLUMNS = 3;
@@ -44,11 +39,22 @@ export default function ImagesScreen() {
             className="rounded-xl overflow-hidden bg-zinc-100"
           >
             {itemSize > 0 && (
-              <ImageWithBoundingBoxes
-                photoPath={item[0]}
-                boxes={item[1].boundingBoxes}
-                imageSize={itemSize}
-              />
+              <View className="flex-1">
+                <ImageWithBoundingBoxes
+                  photoPath={item[0]}
+                  boxes={item[1].boundingBoxes}
+                  imageSize={itemSize}
+                />
+
+                {item[1].isAccepted && (
+                  <View
+                    className="absolute top-2 right-2 bg-green-500 rounded-full p-0.5 shadow-sm"
+                    style={{ elevation: 2 }}
+                  >
+                    <Ionicons name="checkmark-circle" size={18} color="white" />
+                  </View>
+                )}
+              </View>
             )}
           </Pressable>
         )}
